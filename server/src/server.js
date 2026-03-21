d59075af62a24561361552c3e69ba05ab721b3e5
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-03-21 20:49:34 +0530

    feat: Device flow auth for the login part added

--- server/src/server.js
+++ server/src/server.js
@@ -17,9 +17,14 @@ app.use(cors({
     credentials: true,
 }))
 
-app.all("/api/auth/*splat", toNodeHandler(auth))
+app.use("/api/auth", toNodeHandler(auth));
 app.use(express.json());
 
+// Using queries for redirecting
+app.get("/device", async (req, res) => {
+    const { user_code } = req.query
+    res.redirect(`http://localhost:3000/device?user_code=${user_code}`)
+})
 app.get("/health", (req, res) => {
     res.send("Server Health OK")
 })
